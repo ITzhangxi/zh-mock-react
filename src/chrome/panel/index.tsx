3b9622a26f5c09f0ecfa8d66a6c1a419914894b4
zackxizi <zackxizi@qq.com>
2021-12-20 00:42:11 +0800

    feat: add HashRouter

--- src/chrome/panel/index.tsx
+++ src/chrome/panel/index.tsx
@@ -1,13 +1,13 @@
 import './utils/reload';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'antd/dist/antd.less';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
