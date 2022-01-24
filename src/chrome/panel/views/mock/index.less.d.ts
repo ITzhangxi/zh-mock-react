b4b0a23e46318eeaad01a07afff15408091b9b32
zackxizi <zackxizi@qq.com>
2022-01-24 22:56:54 +0800

    feat: 表格高度滚动

--- src/chrome/panel/views/mock/index.less.d.ts
+++ src/chrome/panel/views/mock/index.less.d.ts
@@ -2,7 +2,7 @@
 // Please do not change this file!
 declare namespace IndexLessNamespace {
   export interface IIndexLess {
-    wrapper: string;
+    tableWrapper: string;
   }
 }
 
--- src/chrome/panel/views/mock/index.tsx
+++ src/chrome/panel/views/mock/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './index.less';
 import { Table, Space } from 'antd';
 import Search from './search';
@@ -25,35 +25,38 @@ const columns = [
   },
 ];
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
+const data = new Array(20).fill(1).map((_item, index) => {
+  return {
+    key: index,
+    name: `John Brown ${index}`,
+    age: index,
     address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
+  };
+});
 
 const Mock = () => {
+  const [tableY, setTableY] = useState(0);
+  const tableWrapper = useRef(null);
+  const ro = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+    if (entries.length) {
+      const entry: ResizeObserverEntry = entries[0];
+      const cr = entry.contentRect;
+      setTableY(cr.height - 103);
+    }
+  });
+
+  useEffect(() => {
+    if (tableWrapper.current) {
+      ro.unobserve(tableWrapper.current);
+      ro.observe(tableWrapper.current);
+    }
+  }, [ro]);
   return (
     <>
       <Search />
-      <Table className="box-wrapper" columns={columns} dataSource={data} />
+      <div ref={tableWrapper} className={`box-wrapper ${styles.tableWrapper}`}>
+        <Table columns={columns} dataSource={data} scroll={{ y: tableY }} />
+      </div>
     </>
   );
 };
